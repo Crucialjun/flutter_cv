ecc54a796111b6193d6e5f32d4a700d3aeff93d8
Crucial Jun <nich.otieno@gmail.com>
2022-11-19 23:56:22 +0300

    update main

---
+++ lib/main_dashboard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainDashboard extends StatelessWidget {
+  const MainDashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
