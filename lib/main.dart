4bb77bf4ac919e48f33bd9beddd8d272a12c3981
Nicholas Otieno <nich.otieno@gmail.com>
2024-06-27 20:54:12 +0300

    chore: Add firebase_options.dart to main.dart imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cv/firebase_options.dart';
 
 import 'package:flutter_cv/my_app.dart';
 
